54d3342d0735ae2f06e8e816fa4642c0647ee8e7
Eotix <88321977+Eotix@users.noreply.github.com>
2026-04-08 12:36:27 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+games { 'gta5' }
+description 'EOTIX SCRIPT I DA DUMPVASH I DA NE BALUK SI OSTAWASH'
+client_script {
+    "config.lua",
+    "client_menu.lua",
+	"utils.lua"
+}
+
+
+ui_page "html/ui.html"
+
+files {
+  "html/ui.html",
+  "html/js/*.js",
+  'html/css/*.css',
+  'html/webfonts/*.eot',
+  'html/webfonts/*.svg',
+  'html/webfonts/*.ttf',
+  'html/webfonts/*.woff',
+  'html/webfonts/*.woff2'
+}
